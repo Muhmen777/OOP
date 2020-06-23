2dd6812d6b642e1234168fd07a4d9a2341467853
Muhmen777 <58171494+Muhmen777@users.noreply.github.com>
2020-06-23 12:57:38 +0200

    Add files via upload

---
+++ Aircraft.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Aircraft
+    {
+        //Instance variabler
+        private string airline;
+        private int passengers;
+        private int model;
+
+        //Properties
+        public string Airline
+        {
+            get { return airline; }     //Get
+            set { airline = value; }    //Set value
+        }
+        //Properties
+        public int Passengers
+        {
+            get { return passengers; }  //Get
+            set { passengers = value; } //Set value
+        }
+        //Properties
+        public int Model    
+        {
+            get { return model; }   //Get
+            set { model = value; }  //Set value
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Laver objekter
+            Aircraft PIA = new Aircraft();
+            PIA.Model = 7773;
+            PIA.Passengers = 300;
+            PIA.Airline = "Pakistan International Airlines";
+
+            //Printer ud
+            Console.WriteLine("Model: " + PIA.Model + " Max Passengers: " + PIA.Passengers + " Airline: " + PIA.Airline);
+
+            Console.ReadKey();
+        }
+    }
+}
